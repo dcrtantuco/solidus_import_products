b19e93ace63c6ba087049cc87515fc7e8d761023
darwin christopher tantuco <darwinxciii@yahoo.com>
2019-11-29 12:00:56 +0800

    Fix typo

--- app/services/solidus_import_products/create_product_store.rb
+++ app/services/solidus_import_products/create_product_store.rb
@@ -2,7 +2,7 @@ module SolidusImportProducts
   class CreateProductStore
     def initialize(product_id, store_code)
       @product_id = product_id
-      @store = Spree::Store.find_by(store_code: store_code)
+      @store = Spree::Store.find_by(code: store_code)
     end
 
     def call
@@ -17,7 +17,7 @@ module SolidusImportProducts
     attr_reader :product_id, :store
 
     def product_exist_in_store?
-      sql = "SELECT * spree_products_stores where product_id=#{product_id} and store_id=#{store.id}"
+      sql = "SELECT * FROM spree_products_stores WHERE product_id=#{product_id} AND store_id=#{store.id}"
       execute(sql).count.positive?
     end
 
